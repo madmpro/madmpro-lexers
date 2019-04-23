ee4e6bdfaf921af50ba5fcd14867907a91af820f
madmpro <madm.pro@gmail.com>
2019-04-24 02:14:40 +0300

    Update bsl.py

--- madmpro_lexers/bsl.py
+++ madmpro_lexers/bsl.py
@@ -20,7 +20,7 @@ class Lang1CLexer(RegexLexer):
     """Simple lexer for Language 1C."""
 
     name = 'Language 1C lexer'
-    aliases = ['1c']
+    aliases = ['1c', 'bsl']
     filenames = ['*.bsl', '*.os']
     mimetypes = ['text/x-1c']
 
